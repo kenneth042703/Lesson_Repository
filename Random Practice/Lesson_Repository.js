150afb894c9b5198f0961f821d1c111298331be9
kenneth042703 <78907102+kenneth042703@users.noreply.github.com>
2023-11-20 09:49:13 +0800

    Add files via upload

---
+++ Random Practice/Lesson_Repository.js
@@ -0,0 +1,14 @@
+async function show_result(){
+    const c = document.getElementById("lessonNumOrTitleOrTags").value;
+    if (c !== ""){
+            const a = document.createElement("button");
+            a.setAttribute("class", "btnResult");
+            a.setAttribute("formaction", "#");
+            a.textContent = c;
+            document.getElementById("resultButton").appendChild(a);
+        }
+
+}
+
+// Show the contents of the Lesson with the UI
+// Read data from Pocketbase and make it as Parameters for conditions
